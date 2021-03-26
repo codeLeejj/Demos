2ba6893e67e0fab9e98d52d69cda222423c5bf71
annis <annis.junjielee@gmail.com>
2021-03-26 18:08:31 +0800

    拍照测试

--- app/src/main/java/com/annis/mydemos/MainActivity.kt
+++ app/src/main/java/com/annis/mydemos/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.annis.mydemos
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.annis.mydemos.databinding.ActivityMainBinding
+import com.annis.mydemos.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -28,5 +30,14 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == HomeFragment.INTENT_CAMERA_REQUEST) {
+                val uri = data?.data
+            }
+        }
     }
 }
--- app/src/main/java/com/annis/mydemos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/annis/mydemos/ui/home/HomeFragment.kt
@@ -1,44 +1,169 @@
 package com.annis.mydemos.ui.home
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.annis.mydemos.databinding.FragmentHomeBinding
+import androidx.core.content.FileProvider.getUriForFile
+import java.io.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class HomeFragment : Fragment() {
+open class HomeFragment : Fragment() {
+    companion object {
+        const val INTENT_CAMERA_REQUEST = 1254
+        const val PERMISSION_REQUEST_CAMERA = 2345
+    }
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
         })
+
+        binding.abtCamera.text = "点击-拍照"
+        binding.abtCamera.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+                    openCamera()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                    openCamera()
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.CAMERA),
+                        PERMISSION_REQUEST_CAMERA
+                    )
+                }
+            }
+        }
         return root
+    }
+
+    private var photoFile: File? = null
+    private fun openCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra("outputFormat", "JPEG")
+            val imagePath = File(requireContext().filesDir, "images")
+            if (!imagePath.exists()) {
+                imagePath.mkdir()
+            }
+
+            val format = SimpleDateFormat("yyyy-MM-dd HH:mm").format(Date())
+            photoFile = File(imagePath, "$format.jpg")
+            val contentUri =
+                getUriForFile(requireContext(), "com.annis.mydemos.provider", photoFile!!)
+
+            putExtra(MediaStore.EXTRA_OUTPUT, contentUri)
+        }.let {
+            startActivityForResult(it, INTENT_CAMERA_REQUEST)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            INTENT_CAMERA_REQUEST -> {
+                if (photoFile?.exists() == true) {
+                    var opt = BitmapFactory.Options()
+                    opt.inJustDecodeBounds = true
+                    val bitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath, opt)
+
+                    Bitmap.createBitmap(bitmap,0,0,bitmap.width,bitmap.height, Matrix().apply { setScale(200.0f,400.0f) },true)
+                }
+            }
+            else -> {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            grantResults.forEach {
+                if (it == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    /**
+     * 保存到文件
+     *
+     * @param bitmap    要保存的bitmap
+     * @param outFormat 输出的图片格式
+     * @param callback  保存完bitmap后回调(包含文件保存地址)
+     */
+    fun save(bitmap: Bitmap, outFormat: CompressFormat?, savePath: String) {
+        val destFile: File = File(savePath)
+        if (!destFile.parentFile.exists()) {
+            destFile.parentFile.mkdir()
+        }
+        object : AsyncTask<String?, String?, File?>() {
+            override fun doInBackground(vararg strings: String?): File? {
+                var bos: BufferedOutputStream? = null
+                return try {
+                    bos = BufferedOutputStream(FileOutputStream(destFile))
+                    bitmap.compress(outFormat, 100, bos)
+                    destFile
+                } catch (e: FileNotFoundException) {
+                    null
+                } finally {
+                    try {
+                        if (bos != null) {
+                            bos.flush()
+                            bos.close()
+                        }
+                    } catch (ex: IOException) {
+                        ex.printStackTrace()
+                    }
+                }
+            }
+
+            override fun onPostExecute(file: File?) {
+
+            }
+        }.execute()
     }
 }
