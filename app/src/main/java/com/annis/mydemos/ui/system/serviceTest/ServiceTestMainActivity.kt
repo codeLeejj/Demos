b5884908df082caf79d7ab6d4607c2bfb6d88342
lee <annis.junjielee@gmail.com>
2021-05-20 22:56:47 +0800

    Service 的学习

--- app/src/main/java/com/annis/mydemos/ui/system/serviceTest/ServiceTestMainActivity.kt
+++ app/src/main/java/com/annis/mydemos/ui/system/serviceTest/ServiceTestMainActivity.kt
@@ -1,8 +1,12 @@
 package com.annis.mydemos.ui.system.serviceTest
 
+import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
 import android.widget.Button
 import com.annis.mydemos.R
@@ -19,6 +23,22 @@ class ServiceTestMainActivity : AppCompatActivity() {
         }
         findViewById<Button>(R.id.tvBindService).setOnClickListener {
 
+            mConn = MyConn()
+            bindService(Intent(this, BindServiceTest::class.java), mConn, Service.BIND_AUTO_CREATE)
         }
+    }
+
+    lateinit var mConn: MyConn
+
+    class MyConn : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            //这是意外中断的时候才会调用的方法
+
+        }
+
     }
 }
